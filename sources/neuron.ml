101d793ac97f4b5573b01653437090fcf54c0bcf
antoine becquet <becque_c@pc-becque_c.(none)>
2012-12-09 17:29:11 +0100

    reseau de neurones fini, non teste

--- sources/neuron.ml
+++ sources/neuron.ml
@@ -1,3 +1,30 @@
+let img2matbool img =
+  let xl = (Sdlvideo.surface_info img).Sdlvideo.w and
+      yl = (Sdlvideo.surface_info img).Sdlvideo.h in
+  let mat = Array.make_matrix 30 30 1 in
+  for i = 0 to 29 do
+    for j = 0 to 29 do
+      let x = int_of_float((float_of_int(i*xl)) /. 30.) in
+      let y = int_of_float((float_of_int(j*yl)) /. 30.) in
+      if ((0,0,0) = Sdlvideo.get_pixel_color img x y)
+      then
+        mat.(i).(j) <- 0
+    done
+  done;
+  mat
+      
+let file_names =
+  let lines = ref [] in
+  let chan = open_in "../apprentissage/a.txt" in
+  try
+    while true; do
+      lines := input_line chan::!lines
+    done; [||]
+  with End_of_file ->
+    close_in chan;
+    Types.list2tab !lines
+
+
 class neuron c =
   object (self)
     val name:char = c
@@ -21,6 +48,19 @@ class neuron c =
       done;
       !izgoud
 
+    method learn imgs =
+      let stats_m = Array.make_matrix 30 30 0 in
+      let l = Array.length imgs in
+      let bin_imgs = Array.init l (fun i -> img2matbool imgs.(i-1)) in
+      for i = 0 to 29 do
+        for j = 0 to 29 do
+          for k = 0 to l do
+            stats_m.(i).(j) <- stats_m.(i).(j) + bin_imgs.(k).(i).(j)
+          done;
+          stats_m.(i).(j) <- 100 * stats_m.(i).(j) / l
+        done
+      done
+
   end
 
 
@@ -31,7 +71,7 @@ class network =
     val mutable neurons = [||]
 
     initializer
-      neurons <- Array.init 122 (fun i -> new neuron (char_of_int i))
+      neurons <- Array.init 91 (fun i -> new neuron (char_of_int (i+21)))
 
     method find_char c =
       let mat_c = Array.make_matrix 30 30 0 in
@@ -53,4 +93,15 @@ class network =
           best_r := result
       done;
       !finded_c
+
+    method learn =
+      for c = 0 to 90 do
+        let imgs = Array.init 26 (fun _ -> 
+            Sdlloader.load_image "../apprentissage/z-Verdana.ttf.bmp") in
+        for i = 0 to 25 do 
+          imgs.(i) <- Sdlloader.load_image  
+              ("../apprentissage/"^(string_of_int c)^(file_names.(i)))
+        done;
+        neurons.(c)#learn imgs
+      done
   end
